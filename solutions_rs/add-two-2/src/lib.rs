2b6d853351d184162e208f72c1cf8c605b5f8dee
Theo Yuan <theo.yuan@zoom.us>
2023-11-24 15:45:16 +0800

    LCR142 passed

--- solutions_rs/add-two-2/src/lib.rs
+++ solutions_rs/add-two-2/src/lib.rs
@@ -1,21 +1,58 @@
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
-}gi
-impl Solution {
-    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+}
 
+struct Solution;
+
+impl Solution {
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut new_l = ListNode::new(0);
+        let mut cur = &mut new_l;
+
+        let (mut l1, mut l2) = (l1.as_ref(), l2.as_ref());
+        let mut carry = 0;
+        while let (Some(node1), Some(node2)) = (&l1, &l2) {
+            let mut sum = carry + node1.val + node2.val;
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = Some(Box::new(ListNode::new(sum)));
+            cur = cur.next.as_mut().unwrap();
+            l1 = node1.next.as_ref();
+            l2 = node2.next.as_ref();
+        }
+
+        cur.next = if l1.is_some() {
+            l1.cloned()
+        } else if l2.is_some() {
+            l2.cloned()
+        } else {
+            None
+        };
+
+        while carry == 1 {
+			if cur.next.is_none() {
+				cur.next = Some(Box::new(ListNode::new(0)));
+			}
+            cur = cur.next.as_mut().unwrap();
+            let mut sum = carry + cur.val;
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.val = sum;
+        }
+
+        new_l.next
     }
 }
--- solutions_rs/training4-LCR142/src/lib.rs
+++ solutions_rs/training4-LCR142/src/lib.rs
@@ -1,4 +1,5 @@
 struct Solution;
+
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -12,30 +13,27 @@ impl ListNode {
         ListNode { next: None, val }
     }
 }
+
 impl Solution {
-    pub fn trainning_plan(
-        l1: Option<Box<ListNode>>,
-        l2: Option<Box<ListNode>>,
-    ) -> Option<Box<ListNode>> {
+    pub fn trainning_plan(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         let mut dummy = Some(Box::new(ListNode::new(0)));
         let mut current = &mut dummy;
-
         let mut l1 = l1;
         let mut l2 = l2;
 
-        while let (Some(n1), Some(n2)) = (l1, l2) {
+        while let (Some(n1), Some(n2)) = (l1.as_mut(), l2.as_mut()) {
             let cur_p = current.as_mut().unwrap();
             if n1.val <= n2.val {
-                cur_p.next = Some(n1);
-                l1 = cur_p.as_mut().next;
+                cur_p.next = l1.take();
+                l1 = cur_p.next.as_mut().unwrap().next.take();
             } else {
                 cur_p.next = l2.take();
-                l2 = n2.next.take();
+                l2 = cur_p.next.as_mut().unwrap().next.take();
             }
             current = &mut current.as_mut().unwrap().next;
         }
 
-        current.as_mut().unwrap().next = if l1.is_some() { l1 } else { l2 };
+        current.as_mut().unwrap().next = if l1.is_some() { l1.take() } else { l2.take() };
 
         dummy.unwrap().next
     }
